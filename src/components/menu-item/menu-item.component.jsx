b70f23a061f87fe6cb252d80ade448f2b23bb6ab
Louisfm16 <louisfm16@gmail.com>
2021-01-13 20:17:42 -0800

    Updated our package to the latest version, removed and fixed any vulnerabilities

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,10 +5,10 @@ import './menu-item.styles.scss';
 const MenuItem = ({title, imageUrl, size}) => (
     <div className={`${size} menu-item`}>
         <div 
-            className='background-image' 
+            className='background-image'
             style={{
                 backgroundImage: `url(${imageUrl})`
-            }} 
+            }}
         />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
